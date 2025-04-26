8c40fe30068a25c4d34b160314c7d17eb2276e06
mukuldesai <desai.mu@northeastern.edu>
2025-04-26 13:00:58 -0400

    Remove .next build folder

--- .next/static/chunks/src_app_interview_page_tsx_8d4a8160._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_52cae784._.js",
-  "static/chunks/node_modules_framer-motion_dist_es_92139a2a._.js",
-  "static/chunks/node_modules_3a068baf._.js"
-],
-    source: "dynamic"
-});
